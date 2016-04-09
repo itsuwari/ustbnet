7c5729717326e4ffd02f20d5d4aeee4805e03fbd
Itsuwari no Neko <fraxinasw@gmail.com>
2016-04-10 01:55:28 +0900

    Add py2 support

--- loginv2.py
+++ loginv2.py
@@ -9,7 +9,6 @@ def login(username, passwd, v6):
         'DDDDD': username,
         'upass': passwd
     }
-    # DDDDD=41521008&upass=3334037&0MKKey=123456789&v6ip=2001%3A0da8%3A0208%3Ac247%3Accf8%3A20aa%3A9ed0%3A3498&savePWD=on
     post = urllib.urlencode(post_t)
     post = post.encode('utf-8')
     url = 'http://202.204.48.66/v6'
@@ -36,4 +35,4 @@ try:
     print("Got V6")
 except TimeoutError:
     v6 = ''
-login('41521008', '3334037', v6)
+login('replace with your student id', 'your password', v6)
